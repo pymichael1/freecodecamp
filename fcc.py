d5156520752d6af74e3d21508361b2815db560f9
Michael <hailmichaelking@gmail.com>
2023-06-12 14:02:27 +0000

    Here is a code asking for user input and then reply to user

---
+++ fcc.py
@@ -0,0 +1,6 @@
+#Here is a code asking for user input and then reply to user.
+
+name = input("What's your name? ")
+print("Hello ",name)
+hmihp = input("How may I help you? ")
+print("We will get back to you shortly", name,"!") 
