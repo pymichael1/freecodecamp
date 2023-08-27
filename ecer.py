c43dab988d9d75e4ba7b70025b239b85803f281d
Michael <hailmichaelking@gmail.com>
2023-08-27 14:36:29 +0000

    this is a code that allows you to create an account with your password then allows you login based on the correct password you created

--- ecer.py
+++ ecer.py
@@ -1,17 +1,42 @@
-# This is a program for reading files and counting the number of lines in the file 
-fname = input('Enter file name: ')
-try:
-    fhand = open(fname)
-except:
-    print(fname, 'is an invalid file name')
-    quit()
 
-for line in fhand:
-    line = line.rstrip()
-    count = fhand.read()
-    line = count[ : 300]
-    print('Total lines in this file: ',len(count))
-    print(line.upper())
-    
+# Authentication decorator. Build a password authenticator.
 
-  
+import getpass
+
+print('Create your account here! \n')
+
+user_name = input('Enter your Username: ')
+pass_word = getpass.getpass('Enter your Password: ')
+
+print('done!! \n')
+pass_count = len(pass_word)
+pass_block = int(pass_count) * '*'
+
+print(f'Your username is: {user_name}\nYour password is: {pass_block}\n')
+
+print('Login Here \n')
+
+username = input('Enter Username: ')
+password = getpass.getpass('Enter Password: ')
+
+
+def authenticated(func):
+    def wrapper(*args, **kwargs):
+        # password = getpass.getpass("Verify password: ")
+        if password == pass_word:
+            result = func(*args, **kwargs)
+            return result
+        else:
+            print("Authentication failed.")
+            print('incorrect username or password!!')
+
+    return wrapper
+
+
+@authenticated
+def protected_function():
+    print("Access granted. You're authenticated!")
+
+
+# Test the decorated function
+protected_function()
